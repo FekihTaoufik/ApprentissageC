f3b63319716dd584e43d0b5736bddc874fc73c90
Live session user <tafekih@outlook.fr>
2018-09-27 16:17:06 +0000

    new code

--- S&R/TP2/fork0.c
+++ S&R/TP2/fork0.c
@@ -13,7 +13,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 
-#define MAX_COUNT 100
+#define MAX_COUNT 1000
 #define BUF_SIZE 50
 int main(void)
 {
@@ -28,5 +28,20 @@ int main(void)
     {
         sprintf(buf, "This line is from pid %d, value = %d\n", pid, i);
         write(1, buf, strlen(buf)); /* write pour eviter la bufferisation */
+        //usleep(100000);
     }
+        sprintf(buf, "JE SUIS EN MODE ZOMBIE pid %d\n", pid, i);
+        write(1, buf, strlen(buf)); /* write pour eviter la bufferisation */
+
+
 }
+
+/* 
+1. Comprendre et Expérimenter ce code.
+2. Augmenter d’un facteur 10 le nombre d’itérations (MAX_COUNT).Qu’est-ce qui se passe ?
+3. Vous obtenez un résultat/affichage ? Pouvez vous garantir que tout lancement à venir produira le même
+résultat ?
+4. Une fois que vous avez compris que c’est l’ordonnanceur (scheduler) du système Linux qui décide
+de l’ordre d’exécution, est-ce que vous pouvez proposer un critère qui selon vous est utilisé par cet
+ordonnanceur pour prendre ses décisions ?
+*/
