433970421c25083885da96ec2f35203c076ba499
Fekih <tafekih@outlook.fr>
2018-10-24 22:02:46 +0200

    test

--- POO/TP4-1/src/AppletTest.java
+++
@@ -1,8 +0,0 @@
-
-@SuppressWarnings({ "deprecation", "serial" })
-
-public class AppletTest extends Applet {
-   public void paint(Graphics g) {
-      g.drawString("Welcome in Java Applet.",40,20);
-   }
-}
--- POO/TP4-1/src/test.java
+++ POO/TP4-1/src/test.java
@@ -6,11 +6,26 @@ public class test extends Applet {
 
 	public void paint(Graphics g) {
 		      //g.drawString("Welcome in Java Applet.",40,20);
+		String ch = "JAVA 2012";
+		Code39 c = new Code39(ch);
 		int margeX = 50;
 		int margeY = 50;
-		g.setColor(new Color(0,0,0,255));
-		  	g.fillRect(margeX+0,margeY+0, 10, 150);
-		  	
-		   }
+		int decalage=0;
+		String code = c.message();
+		boolean large,barre;
+		for (int i=0;i<code.length();i++) {
+			
+			barre=i%2==0;
+			large=code.charAt(i)=='1';
+			
+			g.setColor(new Color(0,0,0,barre?255:0));
+			
+		  	g.fillRect(margeX+decalage,margeY, large?6:3, 100);
+		  	 
+		  	decalage += large ? 6 :3 ;
+		}
+		g.drawString('*'+ch+'*', margeX, 220);
+	  	
+	   }
 
 }
