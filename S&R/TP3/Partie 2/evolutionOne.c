d861ba80b24846a773d4140220f85afb0d534961
ll503889 <louis.laiolo@hotmail.com>
2018-10-24 22:07:53 +0200

    correction evolutionOne.c et implementation de evolutionTwo.c

--- S&R/TP3/Partie 2/evolutionOne.c
+++ S&R/TP3/Partie 2/evolutionOne.c
@@ -45,27 +45,18 @@ char** sh_split_line (char* line) { //Parse l'input en utilisant strtok() pour f
 /*======================================================*/
 int sh_execute (char ** args) {
 	int exit_cond;
-	int i;
 	pid_t pid;
 	pid = fork();
-	char *controlParental;
-	const char *forbiddenWords[] = {"mp3", "mp4", "youtube", "avi"};
 	if(pid == -1){
 		perror ("Erreur fork");
 		exit (1);
 	} 
 	if(pid == 0) {
-		for(i = 0; i < sizeof(forbiddenWords); i++){
-			controlParental = strstr(args[0], forbiddenWords[i]);
-			if(controlParental != NULL){
-				perror("PLz fuckin work");
-				exit(1);
-			}
-		}
-		if(execvp(args[0], args) == -1)
+		//printf ("\nPid du premier fils = %d", getpid());
+		if(execvp(args[0], args) == -1){
 			perror("Erreur exec ");
-			exit(1);
-		exit (EXIT_FAILURE);
+			exit (EXIT_FAILURE);
+		}
 	} else {
 		pid = wait(&exit_cond);
 		if (WIFEXITED(exit_cond))
@@ -81,16 +72,39 @@ void sh_loop (void) {
 	char *line ;
 	char **args ;
 	int status ;
+	int check;
 	do {
 		printf("%s", prompt); //Afficher le prompt
 		fflush (stdout); //Nettoyer le buffer de sortie
 		line = sh_read_line(); //Lire la ligne
-		args = sh_split_line(line); //Parse la ligne
-		status = sh_execute (args); //Executer la ligne
+		check = check_forbidden(line);
+		if(check != -1){
+			args = sh_split_line(line); //Parse la ligne
+			status = sh_execute (args); //Executer la ligne
+		} else {
+			printf("Commande interdite !\n");
+		}
 		/*sh_free(line); */
 		/*sh_free(args); */
 	} while (status);
 }
+
+/*======================================================*/
+
+int check_forbidden(char * args){
+	char *forbiddenWords[4] = {"mp3", "mp4", "youtube", "avi"};
+	char *controlParental;
+	int i;
+	for(i = 0; i < 4; i++){
+		controlParental = strstr(args, forbiddenWords[i]);
+		//printf("valeur i : %d\n", i);
+		if(controlParental != NULL)
+			return -1;
+	} 
+	return 1;
+}
+
+/*======================================================*/
 
 int main (int argc, char *argv[]) {
 	// Init : Load config files, if any
