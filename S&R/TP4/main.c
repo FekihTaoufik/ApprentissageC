a2319e5fe80cba73ab64d61c7e2e17cd1f0885b6
Live session user <tafekih@outlook.fr>
2018-11-10 16:57:43 +0100

    1 er partie complété

--- S&R/TP4/main.c
+++ S&R/TP4/main.c
@@ -10,35 +10,27 @@ int main(int argc, char *argv[]){
   FILE *f;
   image *I;
   
-  // if (argc < 3){
-  //   usage();
-  //   return -1;
-  // }
+  if (argc < 3){
+    usage();
+    return -1;
+  }
 
   /* Lecture de l'image */
-  printf("nom du fichier : %s\n",argv[1]);
+
   f = fopen(argv[1],"r");
   if (f == NULL)
     fprintf(stderr,"Erreur d'ouverture du fichier initial \n");
   I = lecture_image(f);
 
-    printf("\n colonnes %d | lignes : %d",I->nc,I->nl);
-
-  // for(int i = 0; i < 100; i++)
-  // {
-  //   printf("%d ",I->pixels[i]);
-  // }
-  
-
   fclose(f);
 
   /* Traitement de l'image */
-  //inverse_image(I);
+  inverse_image(I);
 
   /* Ecriture de l'image */
-  //f = fopen(argv[2],"w");
-  //ecriture_image(f,I);    
-  //fclose(f);
+  f = fopen(argv[2],"w");
+  ecriture_image(f,I);    
+  fclose(f);
   
   return EXIT_SUCCESS;
 }
